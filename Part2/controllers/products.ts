a8125d6dbb8821089ebca288665daea621034b08
DomiiP <95188800+DomiiP@users.noreply.github.com>
2022-09-25 21:29:13 +0200

    There are some problems with POST... need fix

--- Part2/controllers/products.ts
+++ Part2/controllers/products.ts
@@ -1,4 +1,5 @@
-import { Product} from  '../types.ts'
+import {v4} from 'https://deno.land/std/uuid/mod.ts'
+import {Product} from  '../types.ts'
 
 let products: Product[] = [
     {
@@ -25,11 +26,87 @@ let products: Product[] = [
 // @route   GET /api/v1/products
 const getProducts = ({response} : {response: any}) => {
     response.body = {
-        sucess: true,
+        success: true,
         data: products
     }
 }
 
-export {getProducts}
+// @desc    Get single products
+// @route   GET /api/v1/products/:id
+const getProduct = ({params, response} : {params: {id:string}, response: any}) => {
+const product: Product | undefined  = products.find(p => p.id === params.id)
 
-//31:45 - finished for today
+if (product){
+    response.status = 200
+    response.body = {
+        success: true,
+        data: product
+    }
+}else{
+    response.status = 404;
+    response.body = {
+        success: false,
+        msg: 'No product found'
+    }
+}}
+
+// @desc    Add a product
+// @route   POST /api/v1/products
+const addProduct = async({request, response} : {request: any, response: any}) => {
+    const body = await request.body()
+
+    if(!request.hasBody){
+        response.status = 400
+        response.body = {
+            success: false,
+            msg: 'No data'
+        }
+    }else{
+        const product: Product = body.value
+        product.id = v4.generate()
+        products.push(product)
+        response.status = 201 //Everything is OK, something got created
+        response.body = {
+            success: true,
+            date: product
+        }
+
+    }}
+
+// @desc    Update a product
+// @route   PUT /api/v1/products/:id
+const updateProduct = async({params, request, response} : {params: {id:string}, response: any}) => {
+    const product: Product | undefined  = products.find(p => p.id === params.id)
+
+if (product){
+    const body = await request.body()
+    const updateDate : {name?: string; desciption?: string; price?: number} = body.value
+    //More different ways of updating, we use map
+    products = products.map(p => p.id === params.id ? {...p, ...updateData} : p)
+
+    response.status = 200
+    response.body = {
+        success: true,
+        data: products
+    }
+}else{
+    response.status = 404;
+    response.body = {
+        success: false,
+        msg: 'No product found'
+    }
+}
+}
+
+// @desc    Delete a product
+// @route   DELETE /api/v1/products/:id
+const deleteProduct = ({params, response} : {params: {id: string}, response: any}) => {
+    products = products.filter(p => p.id !== params.id)
+    response.body = {
+        success: true,
+        msg: 'Product removed'
+    }
+}
+export {getProducts, getProduct, addProduct, updateProduct, deleteProduct}
+
+//I finished the crash course, but the code has some mistakes with POST... Will look into in the future.
--- Part2/routes.ts
+++ Part2/routes.ts
@@ -1,5 +1,5 @@
 import {Router} from 'https://deno.land/x/oak/mod.ts'
-import { getProducts } from './controllers/products.ts'
+import { getProducts, getProduct, addProduct, updateProduct, deleteProduct} from './controllers/products.ts'
 
 const router = new Router()
 
@@ -7,6 +7,10 @@ const router = new Router()
 //     response.body = 'Hello World'
 // })
 router.get('/api/v1/products', getProducts)
+    .get('/api/v1/products/:id', getProduct)
+    .post('/api/v1/products/:id', addProduct)
+    .put('/api/v1/products/:id', updateProduct)
+    .delete('/api/v1/products/:id', deleteProduct)
 
 
 //Takes arrow function as second parameter
